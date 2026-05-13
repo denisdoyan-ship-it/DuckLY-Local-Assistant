6895d32aa4b07dc9939e1926e57a57d2b9f43841
denisdoyan-ship-it <denisdoyan@gmail.com>
2026-05-14 00:25:56 +0300

    Update DuckLY-L2.5.py

--- den4.ai/DuckLY-L2.5.py
+++ den4.ai/DuckLY-L2.5.py
@@ -1,12 +1,22 @@
+# DuckLY-L2.5: Local Agentic Voice Assistant
 # Copyright (C) 2026 Denis Bdoyan
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#  (at your option) any later version.
+
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Contact information:
+# - Email: denisbdoyan@yandex.ru
+
 
 import tkinter as tk
 from tkinter import Entry, Button, Label
